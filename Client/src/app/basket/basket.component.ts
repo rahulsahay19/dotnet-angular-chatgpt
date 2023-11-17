00059e88bc404385a5ba794a9bdca172f17bdcbb
rahulsahay19 <rahulsahay19@hotmail.com>
2023-11-18 01:41:31 +0530

    initial commit

---
+++ Client/src/app/basket/basket.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-basket',
+  templateUrl: './basket.component.html',
+  styleUrl: './basket.component.scss'
+})
+export class BasketComponent {
+
+}
