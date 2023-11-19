3d755c7f94b825c9a24c5e2e8a698c95e733fcda
rahulsahay19 <rahulsahay19@hotmail.com>
2023-11-19 16:32:44 +0530

    basket page design changes

--- Client/src/app/basket/basket.component.ts
+++ Client/src/app/basket/basket.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BasketService } from './basket.service';
+import { BasketItem } from '../shared/models/basket';
+
 
 @Component({
   selector: 'app-basket',
@@ -7,5 +9,16 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./basket.component.scss']
 })
 export class BasketComponent {
+  constructor(public basketService: BasketService) {}
 
+// Method to extract image name from pictureUrl
+  extractImageName(item: BasketItem): string | null {
+    if (item && item.pictureUrl) {
+      const parts = item.pictureUrl.split('/');
+      if (parts.length > 0) {
+        return parts[parts.length - 1]; // Get the last part after the last /
+      }
+    }
+    return null; // Return null if pictureUrl is not valid
+  }
 }
